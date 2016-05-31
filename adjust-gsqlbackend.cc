ea324d384fbab76acedb13b3ca326f98571ad1a4
wendy-yang <ywf33@sina.cn>
2016-05-31 11:07:39 +0800

    Update adjust-gsqlbackend.cc

--- adjust-gsqlbackend.cc
+++ adjust-gsqlbackend.cc
@@ -3,4 +3,5 @@ Install MySQL;
 Build normal tables for Powerdns, create talbles domainz and create view table domainv and recordz, adjust name lenth of domains,domainz and records;
 Ajust gmysqlbackend.cc, location:pdns-3.3.1/modules/gmysqlbackend/gmysqlbackend.cc;
 Ajust pdns.conf(vi /usr/share/etc/pdns.conf)(instruction: https://doc.powerdns.com/md/authoritative/dnssec/#pdnsutil);
+Do a patch file(diff -Naur pdns-3.3.1.old/modules/gmysqlbackend/gmysqlbackend.cc pdns-3.3.1/modules/gmysqlbackend/gmysqlbackend.cc)
 

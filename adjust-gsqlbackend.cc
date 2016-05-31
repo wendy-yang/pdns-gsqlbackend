42974d0315e1943420af1146ce4cbb3d005aca0f
wendy-yang <ywf33@sina.cn>
2016-05-31 11:04:23 +0800

    Create adjust-gsqlbackend.cc

---
+++ adjust-gsqlbackend.cc
@@ -0,0 +1,6 @@
+Download sound code file from sound code file, version V3.3.1(https://www.powerdns.com);
+Install MySQL;
+Build normal tables for Powerdns, create talbles domainz and create view table domainv and recordz, adjust name lenth of domains,domainz and records;
+Ajust gmysqlbackend.cc, location:pdns-3.3.1/modules/gmysqlbackend/gmysqlbackend.cc;
+Ajust pdns.conf(vi /usr/share/etc/pdns.conf)(instruction: https://doc.powerdns.com/md/authoritative/dnssec/#pdnsutil);
+
